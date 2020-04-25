52650407028f9f90f5f3ad4f699816d3971a9bdd
souravdasbitm <souravdas_bitm@yahoo.co.in>
2020-04-25 20:22:02 +0530

    Runner and refactoring

--- org.starauto.com/src/test/java/stepDefination/LoginStep.java
+++ org.starauto.com/src/test/java/stepDefination/LoginStep.java
@@ -3,23 +3,45 @@ package stepDefination;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.When;
 import cucumber.api.java.en.Then;
+import cucumber.api.PendingException;
 import cucumber.api.java.en.And;
 
 public class LoginStep {
-  @Given("^you are in Given annotation$")
-  public void given() throws Throwable {
-  }
 
-  @When("^you are in When annotation$")
-  public void when() throws Throwable {
-  }
+	@Given("^user is on Home Page$")
+	public void user_is_on_Home_Page() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
 
-  @Then("^you are in Then annotation$")
-  public void then() throws Throwable {
-  }
+	@When("^user clicks onto login option$")
+	public void user_clicks_onto_login_option() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
 
-  @And("^you are in And annotation$")
-  public void and() throws Throwable {
-  }
+	@And("^enters the username$")
+	public void enters_the_username() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@And("^enters the password$")
+	public void enters_the_password() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@And("^clicks onto the login$")
+	public void clicks_onto_the_login() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^user is able to login$")
+	public void user_is_able_to_login() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
 
 }
